e59efe8b1dddc86debe6afd62bc52f7a5601abb3
tigerteamgit <tigerteamtech@gmail.com>
2025-12-14 11:39:16 -0500

    Disable access gate in app.js for development
    Comment out access gate code for development.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ navigator.serviceWorker.register('service-worker.js');
 }
 
 
+// Access gate disabled for development
+/*
 const params = new URLSearchParams(window.location.search);
 if (params.get('access') !== 'YOURTOKEN') {
 document.body.innerHTML = '<h2>Access required</h2>';
 }
+*/
