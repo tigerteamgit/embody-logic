1631278570bed6117d02c757e4858a4e06086ee2
tigerteamgit <tigerteamtech@gmail.com>
2025-12-14 10:47:25 -0500

    Load multiple HTML pages into book element

--- js/book.js
+++ js/book.js
@@ -1,1 +1,23 @@
+const book = document.getElementById('book');
 
+
+fetch('pages/page1.html').then(r => r.text()).then(p1 => {
+fetch('pages/page2.html').then(r => r.text()).then(p2 => {
+fetch('pages/page3.html').then(r => r.text()).then(p3 => {
+
+
+book.innerHTML = p1 + p2 + p3;
+
+
+const pageFlip = new St.PageFlip(book, {
+width: 360,
+height: 640,
+size: 'stretch',
+showCover: true
+});
+
+
+pageFlip.loadFromHTML(document.querySelectorAll('.page'));
+
+
+});});});
