9410453d0c406ded1c4a8d1077bbfe98d3228abf
tigerteamgit <tigerteamtech@gmail.com>
2025-12-14 11:39:52 -0500

    Disable access gate in app.js
    Remove access gate code for development.

--- js/app.js
+++ js/app.js
@@ -3,10 +3,8 @@ navigator.serviceWorker.register('service-worker.js');
 }
 
 
-// Access gate disabled for development
-/*
 const params = new URLSearchParams(window.location.search);
 if (params.get('access') !== 'YOURTOKEN') {
 document.body.innerHTML = '<h2>Access required</h2>';
 }
-*/
+
