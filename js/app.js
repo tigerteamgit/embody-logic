354a696d3993823ec86c715871b21d28051272ba
tigerteamgit <tigerteamtech@gmail.com>
2025-12-14 10:50:03 -0500

    Add service worker registration and access check

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+if ('serviceWorker' in navigator) {
+navigator.serviceWorker.register('service-worker.js');
+}
 
+
+const params = new URLSearchParams(window.location.search);
+if (params.get('access') !== 'YOURTOKEN') {
+document.body.innerHTML = '<h2>Access required</h2>';
+}
